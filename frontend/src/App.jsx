5648b9049e698f8234b25a6fc96ee8d30f801739
kartikmax <kyadav5213@gmail.com>
2024-07-17 19:38:08 +0530

    till the collabarating function

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,79 @@
-import { useState } from 'react'
-import { ChromePicker } from 'react-color'
-import { useDraw } from './hooks/useDraw'
+import { useState,useEffect } from "react";
+import { ChromePicker } from "react-color";
+import { useDraw } from "./hooks/useDraw";
+import {io} from 'socket.io-client'
+import { drawLine } from "./utils/drawLine";
+const socket = io('http://localhost:3001')
 
 const App = () => {
-  const [color, setColor] = useState('#000')
-  const { canvasRef, onMouseDown, clear } = useDraw(drawLine)
+  const [color, setColor] = useState("#000");
+  const { canvasRef, onMouseDown, clear } = useDraw(createLine);
 
-  function drawLine({ prevPoint, currentPoint, ctx }) {
-    const { x: currX, y: currY } = currentPoint
-    const lineColor = color
-    const lineWidth = 5
+  useEffect(() => {
 
-    let startPoint = prevPoint ?? currentPoint
-    ctx.beginPath()
-    ctx.lineWidth = lineWidth
-    ctx.strokeStyle = lineColor
-    ctx.moveTo(startPoint.x, startPoint.y)
-    ctx.lineTo(currX, currY)
-    ctx.stroke()
+    const ctx = canvasRef.current?.getContext('2d')
 
-    ctx.fillStyle = lineColor
-    ctx.beginPath()
-    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI)
-    ctx.fill()
+    socket.emit('client-ready')
+
+    socket.on('get-canvas-state',()=>{
+      if(!canvasRef.current?.toDataURL()) return
+      console.log(canvasRef.current?.toDataURL())
+
+      socket.emit('canvas-state',canvasRef.current.toDataURL())
+
+    })
+
+    socket.on('canvas-state-from-server',(state)=>{
+      console.log('I recived the state ')
+      const img = new Image()
+      img.src = state
+      img.onload = () =>{
+        ctx?.drawImage(img,0,0)
+      }
+    })
+    
+    socket.on('draw-line',({prevPoint, currentPoint ,color})=>{
+
+      if(!ctx) return
+      drawLine({prevPoint,currentPoint,ctx,color})
+
+    })
+  
+   socket.on('clear',clear)
+
+   return ()=>{
+    socket.off('get-canvas-state')
+    socket.off('canvas-state-from-server')
+    socket.off('draw-line')
+    socket.off('clear')
+   }
+
+  }, [])
+  
+
+  function createLine({prevPoint,currentPoint, ctx }){
+    socket.emit('draw-line',({prevPoint,currentPoint,ctx,color}))
+    drawLine({prevPoint,currentPoint,ctx,color})
   }
 
   return (
-    <div className='w-screen h-screen bg-white flex justify-center items-center'>
-      <div className='flex flex-col gap-10 pr-10'>
-        <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
-        <button type='button' className='p-2 rounded-md border border-black' onClick={clear}>
+    <div className="w-screen h-screen bg-white flex justify-center items-center">
+      <div className="flex flex-col gap-10 pr-10">
+        <div>
+          <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
+        </div>
+        <button
+          type="button"
+          className="p-2 rounded-md border border-black"
+          onClick={()=>{socket.emit('clear')}}
+        >
           Clear canvas
+        </button>
+        <button
+          type="button"
+          className="p-2 rounded-md border border-black"
+        >
+         Save Scene
         </button>
       </div>
       <canvas
@@ -38,10 +81,10 @@ const App = () => {
         onMouseDown={onMouseDown}
         width={450}
         height={450}
-        className='border border-black rounded-md'
+        className="border border-black rounded-md"
       />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
